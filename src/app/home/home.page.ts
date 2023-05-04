8cc967ed88e3eea22668329e2457047c18b5eff6
Lucas Nogueira <107207138+Lucas-r-Nogueira@users.noreply.github.com>
2023-05-04 11:56:16 +0000

    Feature função(event) e colors

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,17 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [ //imports
+    IonicModule,
+    CommonModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+  item_selecionado: string = "pokemon";
+
+  selecionarCards(event: any){ // Any vai aceitar qualquer tipo de dados
+    console.log(event)
+    this.item_selecionado = event.detail.value; //pegar variáveis fora da function usa this
+    console.log(this.item_selecionado)
+  }
 }
